82adad5f8cb2b4feb717cea5e26d66013c9a2926
gopoma <gordono@unsa.edu.pe>
2025-05-11 15:41:23 -0500

    Add: Bomb invokes explosion

---
+++ Assets/Scripts/BomboController.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class BomboController : MonoBehaviour
+{
+    [Header("Bomb")]
+    public KeyCode inputKey = KeyCode.LeftShift;
+    public GameObject bombPrefab;
+    public float bombFuseTime = 3f;
+    public int bombAmount = 1;
+    private int bombsRemaining;
+
+    [Header("Explosion")]
+    public Explosion explosionPrefab;
+    public LayerMask explosionLayerMask;
+    public float explosionDuration = 1f;
+    public int explosionRadius = 1;
+
+    private void OnEnable()
+    {
+        bombsRemaining = bombAmount;
+    }
+
+    private void Update()
+    {
+        // Se procede a plantar una bomba al teclear el inputKey y si tienen bombas disponibles
+        if (bombsRemaining > 0 && Input.GetKeyDown(inputKey))
+        {
+            StartCoroutine(PlaceBomb());
+        }
+    }
+
+    private IEnumerator PlaceBomb()
+    {
+        // Obtencíón de la posición del jugador en los ejes x,y
+        Vector2 position = transform.position;
+        
+        // Normalizamos la posición a enteros para evitar problemas con la colisión
+        position.x = Mathf.Round(position.x);
+        position.y = Mathf.Round(position.y);
+
+        //Instacear un objeto bomba en la escena 
+        GameObject bomb = Instantiate(bombPrefab, position, Quaternion.identity);
+        bombsRemaining--;
+
+        // Esperar unos segundos a que la bomba estalle
+        yield return new WaitForSeconds(bombFuseTime);
+
+        position = bomb.transform.position;
+        position.x = Mathf.Round(position.x);
+        position.y = Mathf.Round(position.y);
+
+        Explosion explosion = Instantiate(explosionPrefab, position, Quaternion.identity);
+        explosion.SetActiveRenderer(explosion.start);
+        explosion.DestroyAfter(explosionDuration);
+
+        Explode(position, Vector2.up, explosionRadius);
+        Explode(position, Vector2.down, explosionRadius);
+        Explode(position, Vector2.left, explosionRadius);
+        Explode(position, Vector2.right, explosionRadius);
+
+        Destroy(bomb);
+        bombsRemaining++;
+    }
+
+    private void Explode(Vector2 position, Vector2 direction, int length)
+    {
+        if (length <= 0)
+        {
+            return;
+        }
+
+        position += direction;
+
+        if (Physics2D.OverlapBox(position, Vector2.one / 2f, 0f, explosionLayerMask))
+        {
+            //* ClearDestructible(position);
+            return;
+        }
+
+        Explosion explosion = Instantiate(explosionPrefab, position, Quaternion.identity);
+        explosion.SetActiveRenderer(length > 1 ? explosion.middle : explosion.end);
+        explosion.SetDirection(direction);
+        explosion.DestroyAfter(explosionDuration);
+
+        Explode(position, direction, length - 1);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+}
